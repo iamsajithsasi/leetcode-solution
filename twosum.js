a9879349b7ad3acd26fc26015672c76f2f777e28
sajiths <sajithsasi@qburst.com>
2021-09-14 21:08:59 +0530

    Two sum code updated

--- twosum.js
+++ twosum.js
@@ -32,3 +32,20 @@ var twoSum = function (nums, target) {
   let edIdx = nums.lastIndexOf(ed);
   return [stIdx, edIdx];
 };
+
+// method 2
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum == target) {
+      return [left + 1, right + 1]
+    } else if (sum < target) {
+      left += 1;
+    } else if (sum > target) {
+      right -= 1;
+    }
+  }
+  return []
+};
